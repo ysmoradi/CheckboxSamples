a5de49fcd975cd37dedb6e12b1ed1445cb4e6caa
Yaser Moradi <ysmoradi@outlook.com>
2018-10-07 14:30:13 +0330

    Added binding sample

---
+++ CheckboxSamples/CheckboxSamples/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CheckboxSamples
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsChecked { get; set; }
+
+        public ICommand ChangeIsChecked { get; set; }
+
+        public MainPageViewModel()
+        {
+            ChangeIsChecked = new Command(() => IsChecked = !IsChecked);
+        }
+    }
+}
